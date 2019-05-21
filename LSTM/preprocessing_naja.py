75eb1cea6142d26b8ce0ee6cd35c4ee30d605cef
najacm <naja.marckwardt@gmail.com>
2019-05-21 19:50:09 +0200

    Update preprocessing_naja.py
    everything working in this file. not cleaned yet

--- LSTM/preprocessing_naja.py
+++ LSTM/preprocessing_naja.py
@@ -3,14 +3,15 @@ import numpy as np
 import spacy
 sp = spacy.load('en')
 import nltk
-
+import re
+from nltk import word_tokenize
 from sklearn.model_selection import train_test_split
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 stemmer = SnowballStemmer(language='english')
-
-import os
-from symspellpy.symspellpy import SymSpell  # import the module
+# nltk.download('punkt') # todo: only needs to run ones
+import os #used to look up file
+from symspellpy.symspellpy import SymSpell, Verbosity  # import symspell module
 
 # Import data
 file = open("spamData.txt", "r+")
@@ -18,6 +19,8 @@ file = open("spamData.txt", "r+")
 '''
 1. DATA PREPROCESSING
 '''
+
+# count_re = re.sub(r'[^\w]', ' ', text) # remove special characters/regular expreseions
 
 
 #dataframe of spam data
@@ -54,45 +57,10 @@ validation_samples = int(len(df) - training_samples)
 texts_train = texts[:training_samples]
 labels_train = labels[:training_samples]
 
-filtered_texts =[]
 
-# Different preprocessing tried:
-'''
-# finding misspelled words
-# maximum edit distance per dictionary precalculation
-max_edit_distance_dictionary = 2
-prefix_length = 7
-# create object
-sym_spell = SymSpell(max_edit_distance_dictionary, prefix_length)
+''' NAJA PREPROCESSING '''
 
-# create dictionary using corpus.txt
-if not sym_spell.create_dictionary("C:/users/najam/PycharmProjects/dataScienceGamesCourse/frequency_dictionary_en_82_765.txt"):
-    print("Corpus file not found")
-#check how dictionary looks
-#for key, count in sym_spell.words.items():
-#    print("{} {}".format(key, count))
-'''
-'''
-#remove stopwords in data
-stop_words = set(stopwords.words('english'))
-for w in texts:
-    if w not in stop_words:
-        filtered_texts.append(w)
-
-#Stemming in data, grouping data computing, computer, compute = comput
-for w in texts:
-    filtered_texts.append(stemmer.stem(w))
-
-#Lemmatizing in data, grouping data with actual words
-for text in texts:
-    sentence = sp(text)
-    for word in sentence:
-        filtered_texts.append(word.lemma_) 
-        '''
-
-import os
-from symspellpy.symspellpy import SymSpell, Verbosity  # import the module
-
+#FINDING MISPELLED WORDS
 # maximum edit distance per dictionary precalculation
 max_edit_distance_dictionary = 3
 prefix_length = 7
@@ -106,19 +74,90 @@ count_index = 1  # column of the term frequency in the dictionary text file
 if not sym_spell.load_dictionary(dictionary_path, term_index, count_index):
     print("Dictionary file not found")
 
-#what to look up
-input_term = ("boi giirl trouble")
+test_for_mispelled = texts[:50]
+texts_to_check_mispelled = test_for_mispelled
+max_edit_distance_lookup = 1
 
-# create origional tokenized string to compare with later
-original_string = sym_spell.lookup_compound(input_term,
-                                            0)
-for word in original_string:
-    print(word.term)
+for text in texts_to_check_mispelled:
 
-max_edit_distance_lookup = 3 # max edit distance per lookup (per single word, not per whole input string)
-suggestions = sym_spell.lookup_compound(input_term,
-                                            max_edit_distance_lookup)
+    org_text = text
+    text = text.lower() # since symspell is calculating uppercase words as mispelled, we need to change the string to all lowercase first
+    # print(text)
+    input_term = text
 
-# display suggestion term, edit distance, and term frequency
-for word in suggestions:
-    print("{}, {}, {}".format(word.term, word.distance,word.count))
+    # find number of special characters
+    without_specials = re.sub('[^\^&*$!?,.%]+', '', text) # todo: solution for now is to manualy tell it what characters to look for - alternative: find all non-special characters and substract from original  text (re.findall('[\w]') only_specials = re.sub(r'\w', ' ', text)
+    # print('special characters: ', len(without_specials))
+
+    # find number of digits
+    numbers_in_text = sum(c.isdigit() for c in text)
+    # print("digits in text: ", numbers_in_text)
+
+    # find longest word
+    def find_longest_word(word_list):
+        longest_word = ''
+        longest_size = 0
+
+        for word in word_list:
+            if len(word) > longest_size:
+                longest_word = word
+                longest_size = len(word)
+        return longest_word
+
+    without_specials = re.sub(r'\W', ' ', text)
+    words = without_specials
+    word_list = words.split()
+    longest_word = find_longest_word(word_list)
+
+    # print("longest word: ", len(longest_word))
+
+    #make tokens without special characters or digits
+    strings_to_tokenizer = org_text
+    strings_to_tokenizer = re.sub(r'\W', ' ', strings_to_tokenizer)
+    strings_to_tokenizer = re.sub("^\d+\s|\s\d+\s|\s\d+$", '', strings_to_tokenizer)
+
+
+
+    tokens = word_tokenize(strings_to_tokenizer)
+
+    # find uppercase words todo: needs tokens to work
+    def is_text_upper(user_string):
+        is_upper = False
+        char_counter = len(user_string)
+        if char_counter == 1: # if word is just one letter we don't want it to be calculated as uppercase
+            return False
+        for char in user_string:
+            if 'a' <= char <= 'z':
+                is_upper = False
+                return is_upper
+            elif 'A' <= char <= 'Z': # update so it checks all charactesrs
+                is_upper = True
+        return is_upper
+
+    for token in tokens:
+        testString = is_text_upper(token)
+        # print("uppercase: ", str(testString))
+
+
+    # todo: needs our own toke to work with e.g. don't
+    strings_to_tokenizer2 = text # gets lower case text string
+    strings_to_tokenizer2 = re.sub('[\&*$!?,.%#;=]+', ' ', strings_to_tokenizer2) #todo doesnt know -
+    strings_to_tokenizer2 = re.sub("^\d+\s|\s\d+\s|\s\d+$", ' ', strings_to_tokenizer2)# remove digits
+    tokens2 = word_tokenize(strings_to_tokenizer2)
+
+    no_of_unknown_words = 0
+    print(text)
+    for token in tokens2:
+        results_lookup = sym_spell.lookup(token, Verbosity.TOP, max_edit_distance=2,ignore_token="\d{2}\w*\b|\d+\W+\d+\b|\d\w*\b|[!@£#$%^&*();,.?:{}/|<>]")
+        for word in results_lookup:
+            if word.distance > 0:
+                no_of_unknown_words = no_of_unknown_words + 1
+                print(token, " ", "{}, {}".format(word.term, word.distance)) # for testing
+    print("number of unknown words: ", no_of_unknown_words)
+
+    # look up a line of words
+    ''' suggestions = sym_spell.lookup_compound(input_term, max_edit_distance_lookup)
+    for word in suggestions:
+        print("{}, {}".format(word.term, word.distance))
+    '''
+    print("*****")
